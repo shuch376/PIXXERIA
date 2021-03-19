985906073e40862618f39b7117d664faf9633243
shuch376 <80398596+shuch376@users.noreply.github.com>
2021-03-19 13:11:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+menu_list_array = ["Chiken,Tandori Pizza,Veg Supreme Pizza,Paneer Tikka Pizza,Delux Veggie Pizza,Sweet Corn Pizza,Cheese Pizza"]
+function getmenu(){
+var htmldata;
+htmldata= <ol class ="menulist">"
+menu_list_array.sort();
+for ( var i = 0; index;i<menu_list_array.length;i++)  {htmldata=htmldata+ '<li> + menu_list_array[i] + '</li>'
+{
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+    
+    
+}
